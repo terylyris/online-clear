3fa0a865d43c0728a35b4d28a0f63c9ada61ca5e
terylyris <lyris6384@gmail.com>
2024-03-14 16:52:18 +0300

    worked on SQL tables and filling all required data

--- studentaffairs/models.py
+++ studentaffairs/models.py
@@ -15,27 +15,43 @@ class Activity(models.Model):
 
 
 class Disciplinary(models.Model):
-   Student = models.ForeignKey(Student ,  on_delete=models.CASCADE)
+   student = models.ForeignKey(Student ,  on_delete=models.CASCADE)
    case = models.CharField( max_length=150, unique = True)
    resolved= models.BooleanField(default=False)
+   
+   def __str__(self) -> str:
+       return self.case
+
 
 
 class Damages(models.Model):
-    Student = models.ForeignKey(Student ,  on_delete=models.CASCADE)
+    student = models.ForeignKey(Student ,  on_delete=models.CASCADE)
     damagecaused=models.CharField( max_length=150, unique = True)
     repaired = models.BooleanField(default=False)
 
+    def __str__(self):
+      return self.damagecaused
+
 
 class Hostel(models.Model):
-    Student = models.ForeignKey(Student ,  on_delete=models.CASCADE)
+    student = models.ForeignKey(Student ,  on_delete=models.CASCADE)
     checkindate = models.DateField(auto_now = True)
     checkoutdate = models.DateField(auto_now = True)
     hostelname=models.CharField( max_length=150)
 
+    def __str__(self):
+      return self.hostelname
+    
+
 
 class Games(models.Model):
-    Student = models.ForeignKey(Student ,  on_delete=models.CASCADE)
+    student = models.ForeignKey(Student ,  on_delete=models.CASCADE)
     activity = models.ForeignKey(Activity ,  on_delete=models.CASCADE)
+
+
+    def __str__(self):
+     return self.activity.activityname
+
 
 
 
